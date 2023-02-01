2c73746664285432ae7b290ba4ab3fe262739eb3
Artem0622 <kwilmajor707@gmail.com>
2023-02-01 13:26:19 +0300

    save programm

--- ind.py
+++ ind.py
@@ -1,16 +1,17 @@
-def decor(func):
+def decorator(function):
     def wrapper(*args):
-        a1, a2 = func(*args)
-        return dict(zip(a1, a2))
+        ar1, ar2 = function(*args)
+        return dict(zip(ar1, ar2))
 
     return wrapper
 
 
-def make_two_list(s1, s2):
-    return s1.split(), s2.split()
+@decorator
+def strings_to_lists(a1, a2):
+    return a1.split(), a2.split()
 
 
-s1 = 'Вася Коля Маша Аня Таня'
-s2 = 'мальчик мальчик девочка девочка девочка'
+s1 = 'a b c d f g h l k'
+s2 = '1 2 3 4 5 6 7 8 9'
+print(strings_to_lists(s1, s2))
 
-print(make_two_list(s1, s2))
